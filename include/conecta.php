df3e0c0c25532b23925b0acab517829d1d11198e
enriquefnt <enriquefinetti@gmail.com>
2021-09-22 08:39:37 -0300

    corrige AOP

---
+++ include/conecta.php
@@ -0,0 +1,17 @@
+<?php
+
+try {
+$connect = new PDO('mysql:host=200.45.112.99;dbname=MSP_NUTRICION;
+charset=utf8', 'SiViNSalta', '@#sivin#@salta!%2021&&');
+$connect->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+}
+catch(PDOException $e) {
+$title = 'Ocurrio un error';
+$output = 'No se pudo conectar al servidor: '
+. $e->getMessage() . ' en '
+. $e->getFile() . ':' . $e->getLine();
+
+}
+
+?>
